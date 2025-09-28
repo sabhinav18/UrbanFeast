8033f9586b4af53f9d5cf75fe6fb4a356c94004d
sabhinav18 <156115964+sabhinav18@users.noreply.github.com>
2025-09-28 15:37:11 +0530

    Update List.jsx

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 
 const List = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://urbanfeast-backend.onrender.com"
   const [list,setList] = useState([]);
 
   const fetchList = async () => {
