d9bdb08330a20f0075200f2b359df3bd6e7b53e3
sabhinav18 <156115964+sabhinav18@users.noreply.github.com>
2025-09-28 15:36:11 +0530

    Update Add.jsx

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 
 const Add = () => {
 
-    const url = "http://localhost:4000";
+    const url = "https://urbanfeast-backend.onrender.com";
     const [image, setImage] = useState(false);
     const [data, setData] = useState({
         name:"",
