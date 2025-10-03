8bba820b453961b7597cad454a34077a27370450
sabhinav18 <156115964+sabhinav18@users.noreply.github.com>
2025-10-03 17:00:00 +0530

    Update AppDownload.jsx

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -3,7 +3,7 @@ import { assets } from "../../assets/assets"
 
 const AppDownload = () => {
   return (
-    <div className="px-6 m-auto my-[100px] text-[max(3vw,20px)] text-center font-medium">
+    <div id="app-download" className="px-6 m-auto my-[100px] text-[max(3vw,20px)] text-center font-medium">
         <p>For Better Experience Download <br /> UrbanFeast App</p>
         <div className="flex gap-6 mt-10 justify-center">
             <img className="w-[max(30vw,120px)] max-w-[180px] translate-0.5 cursor-pointer hover:scale-110" src={assets.play_store} alt="" />
